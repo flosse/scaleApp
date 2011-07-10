39412a645188ddea76d6d97ce8532a6739662ec9
Markus Kohlhase <mail@markus-kohlhase.de>
2011-07-10 18:44:49 +0200

    ported i18n module to coffeescript

---
+++ src/plugins/scaleApp.i18n.coffee
@@ -0,0 +1,96 @@
+# Copyright (c) 2011 Markus Kohlhase (mail@markus-kohlhase.de)
+
+# holds the fallback language code
+baseLang = "en"
+
+# PrivateFunction: getBrowserLanguage
+#
+# Returns:
+# (String) the language code of the browser
+getBrowserLanguage = ->
+  (navigator.language or navigator.browserLanguage or baseLang).split("-")[0]
+
+# Holds the current global language code.
+# By default the browsers language is used.
+lang = getBrowserLanguage()
+
+# holds the callback functions
+callbacks = []
+
+# holds the language objects
+i18n = {}
+
+# PrivateFunction: getLanguage
+#
+# Returns:
+# (String) the current language code, that is used globally.
+getLanguage = -> lang
+
+# PrivateFunction: setLanguage
+#
+# Parameters:
+# (String) languageCode  - the language code you want to set
+setLanguage = (languageCode) ->
+  if typeof languageCode is "string"
+    lang = languageCode
+    cb?( languageCode ) for cb in callbacks
+
+# PrivateFunction: subscribe
+#
+# Parameters:
+# (Function) fn - The callback function
+subscribe = (fn) ->
+  if typeof fn is "function"
+    callbacks.push fn
+
+# PrivateFunction: unsubscribe
+#
+# Parameters:
+# (Function) fn - The callback function
+unsubscribe = (fn) ->
+  if typeof fn is "function"
+    callbacks = callbacks.filter (f) -> fn isnt f
+
+# PrivateFunction: _
+#
+# Parameters:
+# (String) instanceId
+# (String) textId
+#
+# Returns
+# (String) the localized string.
+get = (instanceId, text) ->
+  x = i18n[instanceId]
+  x[lang]?[text] ? ( x[lang.substring(0, 2)]?[text] ? ( x[baseLang]?[text] ? text ) )
+
+# PrivateFunction: onInstantiate
+#
+# Parameters:
+# (String) instanceId
+# (object) opt
+onInstantiate = (instanceId, opt) ->
+  if typeof instanceId is "string" and typeof opt is "object"
+    i18n[ instanceId ] = opt.i18n if typeof opt.i18n is "object"
+    console.debug( i18n )
+
+sbPlugin = (sb, instanceId) ->
+  i18n:
+    setLanguage: setLanguage
+    getBrowserLanguage: getBrowserLanguage
+    getLanguage: getLanguage
+    subscribe: subscribe
+    unsubscribe: unsubscribe
+  _: (textId) -> get instanceId, textId
+
+corePlugin =
+  i18n:
+    setLanguage: setLanguage
+    getBrowserLanguage: getBrowserLanguage
+    getLanguage: getLanguage
+    subscribe: subscribe
+    unsubscribe: unsubscribe
+
+scaleApp.registerPlugin "i18n",
+  sandbox : sbPlugin
+  core: corePlugin
+  onInstantiate: onInstantiate
--- test/scaleApp.i18n.js
+++ test/scaleApp.i18n.js
@@ -2,67 +2,80 @@ TestCase("scaleApp.i18n tests", {
 
   setUp: function(){
 
-		this.myLangObj = {
+    this.myLangObj = {
 
-				en: {
-					helloWorld: "Hello world"
-				},
-				de: {
-					helloWorld: "Hallo Welt"
-				},
-				es: {
-					something: "??"
-				}
+        en: {
+          helloWorld: "Hello world"
+        },
+        de: {
+          helloWorld: "Hallo Welt"
+        },
+        es: {
+          something: "??"
+        }
 
-			};
+      };
 
-			this.myModule = function( sb ){
-				return { init: function(){}, destroy: function(){} };
-			};
+      this.myModule = function( sb ){
+        return {
+          init: function(){
 
-			scaleApp.register("module", this.myModule, { i18n: this.myLangObj });
+          },
+          sb: sb,
+          destroy: function(){}
+        };
+      };
 
-			scaleApp.start( "module");
-	},
+      scaleApp.register("module", this.myModule, { i18n: this.myLangObj });
+      scaleApp.start( "module");
 
-	// API
-	"test that scaleApp.i18n namespace is an object": function(){
-		assertEquals( "object", typeof( scaleApp.i18n ) );
-	},
+      this.instance = scaleApp.getInstance("module");
+  },
 
-	"test that getLanguage is a function": function(){
-		assertEquals( "function", typeof( scaleApp.i18n.getLanguage ) );
-	},
+  // API
 
-	"test that setLanguage is a function": function(){
-		assertEquals( "function", typeof( scaleApp.i18n.setLanguage ) );
-	},
+  "test that getLanguage is a function": function(){
+    assertEquals( "function", typeof( scaleApp.i18n.getLanguage ) );
+  },
 
-	"test that getBrowserLanguage is a function": function(){
-		assertEquals( "function", typeof( scaleApp.i18n.getBrowserLanguage ) );
-	},
+  "test that setLanguage is a function": function(){
+    assertEquals( "function", typeof( scaleApp.i18n.setLanguage ) );
+  },
 
-	"test that _ is a function": function(){
-		assertEquals( "function", typeof( scaleApp.i18n._ ) );
-	},
+  "test that getBrowserLanguage is a function": function(){
+    assertEquals( "function", typeof( scaleApp.i18n.getBrowserLanguage ) );
+  },
 
-	// setting and getting languages
+  "test that _ is a function": function(){
+    assertEquals( "function", typeof( this.instance.sb._ ) );
+  },
 
-	"test that setting a language code works": function(){
-		var lang = "en-US";
-		scaleApp.i18n.setLanguage( lang );
-		assertEquals( lang, scaleApp.i18n.getLanguage() );
-	},
+  // setting and getting languages
 
-	"test that _ returns english string if current language is not supported": function(){
-		var lang = "es";
-		scaleApp.i18n.setLanguage( lang );
-    assertEquals( this.myLangObj.en.helloWorld, scaleApp.i18n._("module", "helloWorld") );
-	},
+  "test that setting a language code works": function(){
+    var lang = "en-US";
+    scaleApp.i18n.setLanguage( lang );
+    assertEquals( lang, scaleApp.i18n.getLanguage() );
+  },
 
-	"test that _ returns base language string if current language is not supported": function(){
-		var lang = "de-CH";
-		scaleApp.i18n.setLanguage( lang );
-    assertEquals( this.myLangObj.de.helloWorld, scaleApp.i18n._("module", "helloWorld") );
-	},
+  "test that _ returns english string if current language is not supported": function(){
+    var lang = "es";
+    scaleApp.i18n.setLanguage( lang );
+    assertEquals( this.myLangObj.en.helloWorld, this.instance.sb._("helloWorld") );
+  },
+
+  "test that _ returns base language string if current language is not supported": function(){
+    var lang = "de-CH";
+    scaleApp.i18n.setLanguage( lang );
+    assertEquals( this.myLangObj.de.helloWorld, this.instance.sb._("helloWorld") );
+  },
+
+  "test that subscription to changes works": function(){
+    var fn = function( code ){
+      assertEquals( lang , code );
+    };
+    var lang = "de-CH";
+    scaleApp.i18n.subscribe( fn );
+    scaleApp.i18n.setLanguage( lang );
+  },
 });
