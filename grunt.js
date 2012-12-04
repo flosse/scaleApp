c43f0cdee9371895f514ea41c53189546542f02b
Markus Kohlhase <mail@markus-kohlhase.de>
2012-12-04 16:25:22 +0100

    replaced Cakefile by gunt tasks. closes #28

--- grunt.js
+++ grunt.js
@@ -20,6 +20,13 @@ module.exports = function(grunt) {
           'src/scaleApp.coffee'
         ],
         dest: 'dist/scaleApp.coffee'
+      },
+      full: {
+        src: [
+          'dist/scaleApp.js',
+          'dist/plugins/*.js'
+        ],
+        dest: 'dist/scaleApp.full.js'
       }
     },
     coffee: {
@@ -34,7 +41,7 @@ module.exports = function(grunt) {
     copy: {
       dist: {
         files: {
-          "dist/modules/":"src/modules/*.css"
+          "dist/modules/":"src/modules/*.css",
         }
       }
     },
@@ -42,6 +49,10 @@ module.exports = function(grunt) {
       dist: {
         src: ["dist/scaleApp.js"],
         dest: "dist/scaleApp.min.js"
+      },
+      full: {
+        src: ["dist/scaleApp.full.js"],
+        dest: "dist/scaleApp.full.min.js"
       }
     },
     uglify: {
@@ -59,7 +70,41 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks("grunt-contrib-coffee");
   grunt.loadNpmTasks("grunt-contrib-copy");
-  grunt.registerTask('default', 'concat coffee copy min');
+  grunt.registerTask('default', 'concat:dist coffee concat:full min copy');
 
+  // Quick and dirty task to build a custom bundle
+  // Does s.o. know how to do that properly?
+  grunt.registerTask('custom', 'create a custom bundle', function(){
+
+    if (this.args.length === 0) {
+      grunt.warn(this.name + ", no plugins specified");
+    }
+    else {
+      base = grunt.file.read('./dist/scaleApp.js')
+      plugins = ''
+      for(i=0; i<this.args.length; i++){
+        try{
+          plugins += '\n' + grunt.file.read('./dist/plugins/scaleApp.' + this.args[i] + '.js');
+        }catch(e){
+          console.log(e);
+          grunt.warn('could not find "' + this.args[i] + '" plugin');
+        }
+      }
+      var customBuild = base + '\n' + plugins;
+      grunt.file.write('./dist/scaleApp.custom.js', customBuild);
+      var minify = function(code, resNames){
+        return uglify.uglify.gen_code(
+          uglify.uglify.ast_squeeze(
+            uglify.uglify.ast_mangle(
+              uglify.parser.parse(code)
+              ,{ except: resNames }
+            )
+          )
+        )+';'
+      };
+      var uglify = require("uglify-js");
+      grunt.file.write('./dist/scaleApp.custom.min.js', minify(customBuild));
+    }
+  });
 };
 
